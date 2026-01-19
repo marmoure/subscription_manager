e464afe67991db4beb6b6f934dccda35dcce096d
marmoure <tinky93winky@gmail.com>
2026-01-19 22:29:36 +0100

    fix config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,10 @@
-import type { Config } from 'drizzle-kit'
+import { defineConfig } from 'drizzle-kit'
 
-export default {
+export default defineConfig({
   schema: './src/server/db/schema.ts',
   out: './drizzle',
-  driver: 'better-sqlite',
+  dialect: 'sqlite',
   dbCredentials: {
     url: './db.sqlite',
   },
-} satisfies Config
+})
